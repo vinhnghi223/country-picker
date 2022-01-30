12bf6d9f4ee6e39bc0bd64ac8d1ae42aa3c5cc6b
Nghi Le <vinhnghi223@gmail.com>
2022-01-30 16:18:59 +0200

    added specs

--- src/app/services/countries.service.spec.ts
+++ src/app/services/countries.service.spec.ts
@@ -1,16 +1,176 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Country, DefaultCountry } from '../model';
 
 import { CountriesService } from './countries.service';
 
 describe('CountriesService', () => {
   let service: CountriesService;
-
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  const createMockCountry = (countryName: string) => {
+    return {  ...DefaultCountry, name: countryName}
+  }
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CountriesService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    httpClientSpy.get.and.returnValue(of([]));
+    service = new CountriesService(httpClientSpy);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getCountries', () => {
+    let mockedCountries: Country[];
+    beforeEach(() => {
+      mockedCountries = [
+        createMockCountry('Australia'),
+        createMockCountry('Austria'),
+        createMockCountry('Saudi')
+      ]
+      httpClientSpy.get.and.returnValue(of(mockedCountries));
+    })
+
+    it('should get and make all countries available via $countries', (done: DoneFn) => {
+      service.getCountries()
+      service.countries$.subscribe({
+        next: countries => {
+          expect(countries)
+            .toEqual(mockedCountries);
+          done();
+        },
+        error: done.fail
+      })
+    }); // end of test case
+
   });
+
+  describe('getCountry', () => {
+    
+    it('should return an expected country', (done: DoneFn) => {
+      const expectedCountries: Country[] = [{ 
+        name: 'Australia', 
+        capital: 'Canberra',
+        region: 'Oceania',
+        population: 25687041,
+        flag: '' 
+      }];
+      httpClientSpy.get.and.returnValue(of(expectedCountries));
+      
+      service.getCountry('Australia').subscribe({
+        next: country => {
+          expect(country)
+            .withContext('expected country')
+            .toEqual(expectedCountries[0]);
+          done();
+        },
+        error: done.fail
+      });
+
+    }); // end of test case
+
+    it('should return an empty default country if no country found or request fails', (done: DoneFn) => {
+
+      httpClientSpy.get.and.returnValue(of({status:404, message:'Not Found'}));
+      
+      service.getCountry('Helsinki').subscribe({
+        next: country => {
+          expect(country)
+            .toEqual(DefaultCountry);
+          done();
+        },
+        error: done.fail
+      });
+
+    }); // end of test case
+
+    it('should return DefaultCountry on error case', (done: DoneFn) => {
+      const errorResponse = new HttpErrorResponse({
+        error: { code: 404, message: 'Bad Request' }
+      });
+      httpClientSpy.get.and.returnValue(of(errorResponse));
+
+      service.getCountry('invalid').subscribe({
+        next: country => {
+          expect(country)
+            .toEqual(DefaultCountry);
+          done();
+        },
+        error: done.fail
+      });
+
+    }); // end of test case
+
+  });
+
+  describe('filterCountriesByName', () => {
+
+    beforeEach(() => {
+      const mockedCountries: Country[] = [
+        createMockCountry('Australia'),
+        createMockCountry('Austria'),
+        createMockCountry('Saudi')
+      ]
+      service.countries$ = of(mockedCountries)
+    })
+
+    it('should return a list of filted countries by filter text', (done: DoneFn) => {
+      service.filterCountriesByName('Aus').subscribe({
+        next: countries => {
+          expect(countries)
+            .toEqual(
+              [
+                createMockCountry('Australia'),
+                createMockCountry('Austria')
+              ]
+            );
+          done();
+        },
+        error: done.fail
+      });
+    }) // end of test case
+
+    it('should return an empty list if no match found', (done: DoneFn) => {
+        service.filterCountriesByName('mars').subscribe({
+          next: countries => {
+            expect(countries).toEqual([]);
+            done();
+          },
+          error: done.fail
+        });
+    })
+
+    it('should return empty array on error case', (done: DoneFn) => {
+      const errorResponse = new HttpErrorResponse({
+        error: { code: 404, message: 'Bad Request' }
+      });
+      httpClientSpy.get.and.returnValue(of(errorResponse));
+
+      service.getCountry('invalid').subscribe({
+        next: country => {
+          expect(country)
+            .toEqual(DefaultCountry);
+          done();
+        },
+        error: done.fail
+      });
+
+    }); // end of test case
+
+    it('should return empty array on error case', (done: DoneFn) => {
+      const errorResponse = new HttpErrorResponse({
+        error: { code: 404, message: 'Bad Request' }
+      });
+      httpClientSpy.get.and.returnValue(of(errorResponse));
+
+      service.filterCountriesByName('invalid').subscribe({
+        next: country => {
+          expect(country)
+            .toEqual([]);
+          done();
+        },
+        error: done.fail
+      });
+
+    }); // end of test case
+
+  })
+
 });
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { CountriesMap, CountriesReducerResult, Country, DefaultCountry } from '../model';
+import { Country, DefaultCountry } from '../model';
 
 @Injectable({
   providedIn: 'root'
@@ -22,26 +22,27 @@ export class CountriesService {
     this.http.get<Country[]>(this.BASE_URI + 'all') // HttpClient automatically calls response.json() internally
       .pipe(
         tap(countries => this.countriesSubject.next(countries.map(this.transformCountry))),
-        catchError(this.handleError<Country[]>('getCountries', []))
+        catchError((err) => of([]))
       )
       .subscribe();
-  }
-
-  filterCountriesByName(countryName: string): Observable<Country[]>{
-    return this.countries$.pipe(
-        map(countries => countries.filter( country => {
-          return country.name.toLowerCase().includes(countryName.toLowerCase());
-        }),
-        catchError(this.handleError<Country[]>('filterCountriesByName', []))
-      )
-    );
   }
 
   getCountry(countryName: string) {
     return this.http.get<Country[]>(this.BASE_URI + 'name/' + countryName)
       .pipe(
-        map(countries => this.transformCountry(countries[0]))
+        map(countries => this.transformCountry(countries[0])),
+        catchError((err) => of(DefaultCountry))
       )
+  }
+
+  filterCountriesByName(filterdCountryName: string): Observable<Country[]>{
+    return this.countries$.pipe(
+        map(countries => countries.filter( country => {
+          return country.name.toLowerCase().includes(filterdCountryName.toLowerCase());
+        }),
+        catchError((err) => of([]))
+      )
+    );
   }
 
   // UTILITIES
@@ -57,25 +58,6 @@ export class CountriesService {
     } else {
       return DefaultCountry
     }
-
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   *
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 
 }
